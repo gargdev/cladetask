655226c68dd507360da7c2dce1d08d19b0487399
gargdev <anooppandey937@gmail.com>
2024-06-29 00:16:29 +0530

    sidebar fixed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "./components/shared/Layout";
-import DashboardPage from "./components/DashboardPage";
-import Products from "./components/Products";
 import Applicants from "./components/Applicants";
 import JobPreview from './components/JobPreview'
 import Match from "./components/Match";
@@ -11,10 +9,8 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route index element={<DashboardPage/>} />
-          <Route path="/product" element={<Products/>} />
+          <Route index element={<JobPreview/>}/>
           <Route path="/applicant" element={<Applicants/>} />
-          <Route path="/preview" element={<JobPreview/>} />
           <Route path="/match" element={<Match/>} />
           <Route path="/message" element={<Messages/>} />
         </Route>
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const Navigation = () => {
   return (
     <div className='flex items-center space-x-8 h-10 shadow px-40 py-6'>
-        <Link to="/preview"><div className="text-gray-600 font-semibold hover:underline underline-offset-[18px] decoration-2 hover:text-orange-500 cursor-pointer">Job Preview</div></Link>
+        <Link to="/"><div className="text-gray-600 font-semibold hover:underline underline-offset-[18px] decoration-2 hover:text-orange-500 cursor-pointer">Job Preview</div></Link>
         <Link to="/applicant"><div className="text-gray-600 font-semibold hover:underline underline-offset-[18px] decoration-2 hover:text-orange-500 cursor-pointer">Applicants</div></Link>
         <Link to="/match"><div className="text-gray-600 font-semibold hover:underline underline-offset-[18px] decoration-2 hover:text-orange-500 cursor-pointer">Match</div></Link>
         <Link to="/message"><div className="text-gray-600 font-semibold hover:underline underline-offset-[18px] decoration-2 hover:text-orange-500 cursor-pointer">Messages</div></Link>
