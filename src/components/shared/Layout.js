4e1712b0106a2f4114d4c4ad4b5a905acac5b990
sumit1298 <sumitkesarwani2323@gmail.com>
2024-06-28 23:52:05 +0530

    Changes in Layout

--- src/components/shared/Layout.js
+++ src/components/shared/Layout.js
@@ -9,10 +9,13 @@ export default function Layout() {
       <div>{<Header/>}</div>
       <div className="">
       <div>{<Navigation/>}</div>
-      </div>
-      <div className="flex items-center justify-around">
+      </div >
+      <div className="flex py-3 px-5 flex-col lg:flex-row p-4">
       <div className="w-[70%] justify-center">{<Outlet/>}</div>
-      <div className="w-[30%]">{<Sidebar/>}</div>
+       <div className="flex items-center">
+            <div className="h-full border-l-2 border-gray-300"></div>
+          </div>
+      <div className="w-[30%] flex flex-col p-4 bg-white">{<Sidebar/>}</div>
       </div>
       
     </div>
